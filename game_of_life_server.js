615bd9fd70423949e44ab50ec43ab9d3c9933727
vcrazy <vcrazy@abv.bg>
2012-12-17 23:10:26 +0200

    1st working version

--- game_of_life_server.js
+++ game_of_life_server.js
@@ -1,16 +1,85 @@
 var app = require('http').createServer(),
 	io = require('socket.io').listen(app),
-	port = 9009;
+	port = 9009,
+	width = 0,
+	height = 0;
 
 app.listen(port);
 
-function calculate_next(){
-	return '111000';
+function calculate_next(current_data){
+	var current = current_data.data,
+		next_state = [];
+
+	width = current_data.width;
+	height = current_data.height;
+
+	for(var i = 0; i < height * width; i++){
+		next_state[i] = live_or_die(current[i] * 1, get_neighbours_number(i, current)) * 1;
+	}
+
+	next_state = next_state.join('');
+
+	return {
+		data: next_state,
+		changed: next_state !== current
+	}
+}
+
+function live_or_die(current_state, neighbours_number){
+	if(current_state && (neighbours_number === 2 || neighbours_number === 3)){
+		return true;
+	}
+
+	if(!current_state && neighbours_number === 3){
+		return true;
+	}
+
+	return false;
+}
+
+function get_neighbours_number(cell_number, current){
+	var row = Math.floor(cell_number / width),
+		col = cell_number % width,
+		neighbours = 0;
+
+	if(row - 1 >= 0){
+		if(col - 1 >= 0){
+			neighbours += current[cell_number - 1 - width] * 1;
+		}
+
+		neighbours += current[cell_number - width] * 1;
+
+		if(col + 1 < width){
+			neighbours += current[cell_number + 1 - width] * 1;
+		}
+	}
+
+	if(col - 1 >= 0){
+		neighbours += current[cell_number - 1] * 1;
+	}
+
+	if(col + 1 < width){
+		neighbours += current[cell_number + 1] * 1;
+	}
+
+	if(row + 1 < width){
+		if(col - 1 >= 0){
+			neighbours += current[cell_number - 1 + width] * 1;
+		}
+
+		neighbours += current[cell_number + width] * 1;
+
+		if(col + 1 < width){
+			neighbours += current[cell_number + 1 + width] * 1;
+		}
+	}
+
+	return neighbours;
 }
 
 io.sockets.on('connection', function(socket){
 	socket.on('get_next', function(current){
-		var next = calculate_next();
+		var next = calculate_next(current);
 		socket.emit('next', next);
 	});
 });
--- scripts.js
+++ scripts.js
@@ -1,9 +1,22 @@
-width = 10,
+width = 20,
 height = 10;
+auto = false;
 
 var socket = io.connect('http://localhost:9009');
 socket.on('next', function(data){
-	console.log(data);
+	var data_arr = data.data.split('');
+
+	$.each($('.dot'), function(i, dot){
+		$(this).toggleClass('live', data_arr[i] == 1);
+	});
+
+	if(!data.changed){
+		$('.auto_stop').click();
+	}else if(auto){
+		setTimeout(function(){
+			auto && $('.get_next').click();
+		}, 250);
+	}
 });
 
 $(document).ready(function(){
@@ -11,21 +24,47 @@ $(document).ready(function(){
 		var current = '';
 
 		$.each($('.dot'), function(dot){
-			current += ($(this).hasClass('live') * 1);
+			current += ($(this).hasClass('live') * 1 + '');
 		});
 
-		socket.emit('get_next', current);
+		socket.emit('get_next', {
+			data: current,
+			width: width,
+			height: height
+		});
+	});
+
+	$('.auto_start').click(function(){
+		auto = true;
+		$('.get_next').click();
+	});
+
+	$('.auto_stop').click(function(){
+		auto = false;
 	});
 
 	$('.dot').live('click', function(){
 		$(this).toggleClass('live');
 	});
 
+	$('.reset').click(function(){
+		window.location.reload();
+	});
+
+	$('.random').click(function(){
+		$.each($('.dot'), function(){
+			$(this).toggleClass('live', Math.floor(Math.random() * 2) === 1);
+		});
+	});
+
 	function generate_grid(){
-		for(var i = 0; i < height; i++){
-			for(var j = 0; j < width; j++){
-				$('#grid').append('<div class="dot">&nbsp;</div>');
-			}
+		$('#grid').css({
+			width: 50 * width,
+			height: 50 * height
+		});
+
+		for(var i = 0; i < height * width; i++){
+			$('#grid').append('<div class="dot">&nbsp;</div>');
 		}
 	}
 
