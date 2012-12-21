e27f36d4fc9c972e1d8504ddc4dcb7d21127ec5f
vcrazy <vcrazy@abv.bg>
2012-12-21 22:23:33 +0200

    v2.0

---
+++ client/scripts.js
@@ -0,0 +1,222 @@
+board = {},
+cell = {
+	width: 25,
+	height: 16
+},
+timing = {
+	all: []
+},
+
+auto = false,
+interval = 120, // in ms
+
+socket = io.connect('http://game-of-life.jit.su/');
+
+$(document).ready(function(){
+// ACTIONS
+	$('.get_next').click(function(){
+		board.get_next();
+	});
+
+	$('.auto_start').click(function(){
+		auto = true;
+		$('.get_next').click();
+	});
+
+	$('.auto_stop').click(function(){
+		auto = false;
+	});
+
+	$('.dot').live('click', function(){
+		cell.click($(this));
+	});
+
+	$('.reset').click(function(){
+		window.location.reload();
+	});
+
+	$('.random').click(function(){
+		board.randomize();
+	});
+// END ACTIONS
+
+// SOCKET
+	socket.on('next', function(data){
+		timing.stop();
+		board.receive(data);
+	});
+// END SOCKET
+
+// CELL
+	cell.click = function(selected_cell){
+		$(selected_cell).toggleClass('live');
+		board.recalculate_live_dots($(selected_cell).hasClass('live'));
+	};
+
+	cell.change = function(cell, live){
+		$(cell).toggleClass('live', live);
+	};
+// END CELL
+
+// GRID
+	var Grid = {
+		generate: function(){
+			for(var i = 0; i < cell.height * cell.width; i++){
+				$('#grid').append('<div class="dot">&nbsp;</div>');
+			}
+		},
+		change: function(){
+			var dot = {
+				width: $('.dot:first').css('width').split('px')[0] * 1 + 2,
+				height: $('.dot:first').css('height').split('px')[0] * 1 + 2
+			};
+
+			$('#grid').css({
+				width: dot.width * cell.width,
+				height: dot.height * cell.height
+			});
+
+			$('#cellall').html(cell.width * cell.height);
+		}
+	};
+// END GRID
+
+// BOARD
+	board.get_next = function(){
+		socket.emit('get_next', {
+			data: board.get_cells(),
+			width: cell.width,
+			height: cell.height
+		});
+		timing.start();
+	};
+
+	board.get_cells = function(){
+		var current = '';
+
+		$.each($('.dot'), function(){
+			current += ($(this).hasClass('live') * 1 + '');
+		});
+
+		return current;
+	};
+
+	board.randomize = function(frequency){
+		frequency = frequency || 0.25;
+
+		$.each($('.dot'), function(){
+			cell.change($(this), Math.floor(Math.random() / frequency) === 0);
+		});
+
+		board.set_live(0); // on randomize set 0 live
+	};
+
+	board.change_live_dots = function(one_cell_value){
+		var live_dots = $('#celllive').html() * 1 + (one_cell_value ? 1 : -1);
+	
+		board.set_live_dots_number(live_dots);
+
+		return live_dots
+	};
+
+	board.recalculate_live_dots = function(){
+		var live_dots = $('.dot.live').length;
+
+		board.set_live_dots_number(live_dots);
+
+		return live_dots;
+	};
+
+	board.set_live_dots_number = function(live_dots){
+		$('#celllive').html(live_dots);
+	};
+
+	board.set_live = function(live){
+		live = board.beautify_number(live, $('#cellall').html().length);
+
+		$('#celllive').html(live);
+
+		return live;
+	};
+
+	board.beautify_number = function(number, target_length){
+		return number.length < target_length ? board.beautify_number('0' + number, target_length) : number;
+	};
+
+	board.check_max_live = function(new_number){
+		var current_live = $('#cellmaxlive').html() * 1;
+
+		if(new_number > current_live){
+			$('#cellmaxlive').html(new_number);
+		}
+	};
+
+	board.change_cells = function(values){
+		$.each($('.dot'), function(i, dot){
+			cell.change($(this), values[i] == 1);
+		});
+	};
+
+	board.receive = function(data){
+		if(!data.changed){
+			return $('.auto_stop').click();
+		}
+
+		board.init_tasks(data.data.split(''));
+
+		auto && setTimeout(function(){
+			auto && board.get_next();
+		}, interval);
+	};
+
+	board.init_tasks = function(data){
+		board.change_cells(data);
+
+		var new_live_number = board.recalculate_live_dots();
+		board.set_live(new_live_number);
+		board.check_max_live(new_live_number);
+	};
+
+	board.generate_grid = function(){
+		Grid.generate();
+		Grid.change();
+	};
+
+	board.generate_grid();
+// END BOARD
+
+// TIMING
+	timing.start = function(){
+		timing.time = + new Date;
+	};
+
+	timing.stop = function(){
+		timing.all.push(+ new Date - timing.time);
+		timing.time = null;
+	};
+
+	timing.log = function(){
+		var all = timing.all,
+			sum = avg = min = max = 0,
+			count = all.length;
+
+		for(var i in all){
+			sum += all[i];
+			min = min ? Math.min(min, all[i]) : all[i];
+			max = Math.max(max, all[i]);
+		}
+
+		avg = sum / count;
+
+		console.log({
+			info: 'All times are in ms',
+			sum: sum,
+			count: count,
+			avg: Math.round(avg),
+			min: min,
+			max: max,
+			all: all
+		});
+	};
+// END TIMING
+});
